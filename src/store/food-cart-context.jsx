7da8196a6b9ef6ff420fa77a4c148afac1a93287
stevopritchard <stephen.a.pritchard@gmail.com>
2025-06-03 19:17:52 +0100

    Add item dispatch action to reducer function.
    Reducer function 'ADD_ITEM' dispatch action handles the addition of menu items to the order object in state, and also manages the quantity when the same dish is ordered more than once.

--- src/store/food-cart-context.jsx
+++ src/store/food-cart-context.jsx
@@ -7,42 +7,69 @@ export const CartContext = createContext({
   updateItemQuantity: () => {},
 });
 
-// function foodCartReducer(state, action) {
-//   if (action.type === 'ADD ITEM') {
-//     const updateItems = [...state.items];
-
-//     const existingCartItemIndex = updateItems.findIndex(
-//       (cartItem) => cartItem.id === action.payload
-//     );
-//     const existingCartItem = updateItems[existingCartItemIndex];
-
-//     if (existingCartItem) {
-//       const updatedItem = {
-//         ...existingCartItem,
-//         quantity: existingCartItem.quantity + 1,
-//       };
-//       updateItems[existingCartItemIndex] = updateItems;
-//     } else {
-//       // const product =
-//     }
+// might still need an analogous function that gets saved order
+// async function getMeals() {
+//   try {
+//     const meals = await fetchAvailableMeals();
+//     console.log(meals);
+//     return {
+//       items: [],
+//       availableMeals: meals,
+//     };
+//   } catch (error) {
+//     console.log(error);
 //   }
 // }
 
-export default function CartContextProvider({ children }) {
-  // const [foodCartState, foodCartDispatch] = useReducer(foodCartReducer, {
-  //   items: [],
-  // });
+function foodCartReducer(state, action) {
+  if (action.type === 'ADD_ITEM') {
+    const updatedItems = [...state.items];
 
+    const existingCartItemIndex = updatedItems.findIndex(
+      (orderItem) => orderItem.id === action.payload.id
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      // need to access the quantity of the item on the current order
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({
+        id: action.payload.id,
+        name: action.payload.name,
+        price: action.payload.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      items: updatedItems,
+    };
+  }
+}
+
+export default function CartContextProvider({ children }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availableMeals, setAvailableMeals] = useState([]);
-  const [currentOrder, setCurrentOrder] = useState({ items: [] });
+  const [foodCartState, foodCartDispatch] = useReducer(
+    foodCartReducer,
+    {
+      items: [],
+    }
+    // getMeals
+  );
 
   useEffect(() => {
     async function getMeals() {
       setIsFetching(true);
       try {
-        const meals = await fetchAvailableMeals();
-        setAvailableMeals(meals);
+        fetchAvailableMeals().then((meals) => {
+          setAvailableMeals(meals);
+        });
       } catch (error) {
         console.log(error);
       }
@@ -52,37 +79,20 @@ export default function CartContextProvider({ children }) {
   }, []);
 
   useEffect(() => {
-    console.log(currentOrder);
-  }, [currentOrder]);
+    console.log(foodCartState.items);
+  }, [foodCartState.items]);
 
-  function addToOrder(id) {
-    // shallow copy of array item
-    const menuItem = availableMeals.filter((meal) => meal.id === id)[0];
-
-    // boolean, but need to access the quantity
-    // prop on this array item via this same method
-    const isOnOrder =
-      currentOrder.items.filter((orderItem) => orderItem.id === id).length > 0;
-
-    if (isOnOrder) {
-      // need to access the quantity of the item on the current order
-      const updatedQuantity = currentOrder.items.map((orderItem) => {
-        if (orderItem.id === id) {
-          orderItem.quantity += 1;
-        }
-        return orderItem;
-      });
-      setCurrentOrder({ items: updatedQuantity });
-    } else {
-      menuItem.quantity = 1;
-      setCurrentOrder({ items: [...currentOrder.items, menuItem] });
-    }
+  function addItemToCart(meal) {
+    foodCartDispatch({
+      type: 'ADD_ITEM',
+      payload: meal,
+    });
   }
 
   const contextValue = {
     meals: availableMeals,
     isFetching: isFetching,
-    addItemtoCart: addToOrder,
+    addItemtoCart: addItemToCart,
   };
 
   return (
